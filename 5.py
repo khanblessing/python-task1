e7a1a1cab77463699faf7e684fa755d3fec9a80f
Khan Blessing <khanblessing2003@gmail.com>
2023-07-05 15:22:27 +0200

    code updated

--- 5.py
+++ 5.py
@@ -1,7 +1,7 @@
 menbers = ["Neolla", "Melvis", "Goodness", "Joy", "Mark"]
 menbers.insert(0, "Godwill")
 menbers.append( "Fadila")
-menbers.insert(3, "Bella")
+menbers.insert(3, "Bella") #print actual middle item by dividing the list size by 2 even list, for odd list, use math.floor + 1;
 print(menbers)
 guest = ["Reymond", "Jeff", "Louis"]
 print(guest) 
